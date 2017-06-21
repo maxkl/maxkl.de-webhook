97023e9336cd9fc5cfb5664de65103f69e10cdd6
maxkl <max@maxkl.de>
2017-06-21 16:44:00 +0200

    Pull repo

--- index.js
+++ index.js
@@ -25,6 +25,41 @@ handler.on('error', function (err) {
 	console.error(err);
 });
 
+function exec(cmd, options) {
+	return new Promise(function (resolve, reject) {
+		child_process.exec(cmd, options, function (err, stdout, stderr) {
+			if(err) {
+				err.stdout = stdout;
+				err.stderr = stderr;
+				reject(err);
+				return;
+			}
+
+			resolve({ stdout, stderr });
+		});
+	});
+}
+
+async function updateRepo(repoDir, remote, branch) {
+	console.log(`Working directory: ${repoDir}`);
+
+	console.log(`git fetch ${remote}`);
+	await exec(`git fetch ${remote}`, { cwd: repoDir });
+
+	console.log(`git reset --hard ${remote}/${branch}`);
+	await exec(`git reset --hard ${remote}/${branch}`, { cwd: repoDir });
+
+	console.log(`git submodule update --init --recursive`);
+	await exec(`git submodule update --init --recursive`, { cwd: repoDir });
+}
+
 handler.on('push', function (event) {
-	console.log('Push to %s', event.payload.repository.name);
+	updateRepo(config.repo.directory, config.repo.remote, config.repo.branch)
+		.then(function () {
+			console.log('Repo updated');
+		})
+		.catch(function (err) {
+			// TODO: better error reporting
+			console.error(err);
+		});
 });
